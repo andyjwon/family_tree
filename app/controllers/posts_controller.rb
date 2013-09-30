48f8d60e9745eb80dc7dd73b7e9cb2908c03387e
Andrew Won <andyjwon@gmail.com>
2013-09-29 17:07:43 -0700

    Added edit post ability for message board

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,5 +20,19 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
 end
 
